d4afe2e0a5042cf9ec67c297a91dd81e98ce0093
wabray17 <wabray17@gmail.com>
2016-08-26 12:06:06 -0500

    Completed weekend work.

--- algo/reverse_a_string.rb
+++ algo/reverse_a_string.rb
@@ -3,7 +3,17 @@
 # comes with Ruby!
 
 def reverse_a_string(string)
-
+  array = string.split('')
+  index_array = []
+  final_array = []
+  array.each do |letter|
+    index_array << array.index(letter)
+  end
+  index_array.sort! { |a, b| b <=> a }
+  index_array.each do |index|
+    final_array << array[index]
+  end
+  final_array.join
 end
 
 # Driver code - don't touch anything below this line.
--- oop/rectangle.rb
+++ oop/rectangle.rb
@@ -1,6 +1,19 @@
 # Create a Rectangle class with readable width and height attributes
 # and a method to calculate its area.
 
+class Rectangle
+  attr_reader :width, :height
+  def initialize(width, height)
+    @width = width
+    @height = height
+  end
+
+  def area 
+    @width * @height
+  end
+end
+
+test = Rectangle.new(10, 30)
 
 
 # Driver code - don't touch anything below this line.
